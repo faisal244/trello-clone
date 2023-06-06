89f4724cad3a3c7f5a2e844b7d231d465b0a4bde
Faisal Mohammed <m.faisal244@gmail.com>
2023-06-06 22:10:25 +0100

    Create getTodosGroupedByColumn.ts

---
+++ lib/getTodosGroupedByColumn.ts
@@ -0,0 +1,53 @@
+import { database } from "@/appwrite";
+
+export const getTodosGroupedByColumn = async () => {
+	const data = await database.listDocuments(
+		process.env.NEXT_PUBLIC_DATABASE_ID!,
+		process.env.NEXT_PUBLIC_TODOS_COLLECTION_ID!
+	);
+
+	const todos = data.documents;
+
+	const columns = todos.reduce((acc, todo) => {
+		if (!acc.get(todo.status)) {
+			acc.set(todo.status, {
+				id: todo.status,
+				todos: [],
+			});
+		}
+
+		acc.get(todo.status)!.todos.push({
+			$id: todo.$id,
+			$createdAt: todo.$createdAt,
+			title: todo.title,
+			status: todo.status,
+			...(todo.image && { Image: JSON.parse(todo.image) }),
+		});
+
+		return acc;
+	}, new Map<TypedColumn, Column>());
+
+	// if column doesnt have inprogress, todo and done, and them with empty todos
+	const columnTypes: TypedColumn[] = ["todo", "in-progress", "done"];
+	for (const columnType of columnTypes) {
+		if (!columns.get(columnType)) {
+			columns.set(columnType, {
+				id: columnType,
+				todos: [],
+			});
+		}
+	}
+
+	// Sort columns by columnTypes
+	const sortedColumns = new Map(
+		Array.from(columns.entries()).sort(
+			(a, b) => columnTypes.indexOf(a[0]) - columnTypes.indexOf(b[0])
+		)
+	);
+
+	const board: Board = {
+		columns: sortedColumns,
+	};
+
+	return board;
+};
