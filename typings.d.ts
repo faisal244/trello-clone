c22431b380e4f0610d939b8f87de9e53bd80a3db
Faisal Mohammed <m.faisal244@gmail.com>
2023-06-08 14:31:13 +0100

    Update typings.d.ts
    Fixed a typo

--- typings.d.ts
+++ typings.d.ts
@@ -2,7 +2,7 @@ interface Board {
 	columns: Map<TypedColumn, Column>;
 }
 
-type TypedColumn = "todo" | "in-progress" | "done";
+type TypedColumn = "todo" | "inprogress" | "done";
 
 interface Column {
 	id: TypedColumn;
