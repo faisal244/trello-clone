2466dc16211cfc77ec5adad55f141527eb027460
Faisal Mohammed <m.faisal244@gmail.com>
2023-06-13 23:58:01 +0100

    Update Board.tsx

--- components/Board.tsx
+++ components/Board.tsx
@@ -34,7 +34,34 @@ function Board() {
 				columns: rearrangedColumns,
 			});
 
-}	};
+		}
+		const columns = Array.from(board.columns);
+		const startColIndex = columns[Number(source.droppableId)];
+		const finishColIndex = columns[Number(destination.droppableId)];
+
+		const startCol: Column = {
+			id: startColIndex[0],
+			todos: startColIndex[1].todos,
+		}
+
+		const finishCol = {
+			id: finishColIndex[0],
+			todos: finishColIndex[1].todos,
+		}
+
+		if (!startCol || finishCol) return;
+
+		if (source.index === destination.index && startCol === finishCol) return;
+
+		const newTodos = startCol.todos;
+		const [todoMoved] = newTodos.splice(source.index, 1);
+
+		if (startCol.id === finishCol.id) {
+			// Same column task drag
+		} else {
+// Dragging to another column
+		}
+	};
 	return (
 		<DragDropContext onDragEnd={handleOnDragEnd}>
 			<Droppable
