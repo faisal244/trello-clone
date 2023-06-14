2b2c184d53db63da441b09a31ec1e7047f8b1979
Faisal Mohammed <m.faisal244@gmail.com>
2023-06-14 23:58:46 +0100

    Update Column.tsx

--- components/Column.tsx
+++ components/Column.tsx
@@ -3,6 +3,7 @@
 import { PlusCircleIcon } from "@heroicons/react/24/solid";
 import { Draggable, Droppable } from "react-beautiful-dnd";
 import TodoCard from "./TodoCard";
+import { useBoardStore } from "@/store/BoardStore";
 
 type Props = {
 	id: TypedColumn;
@@ -16,6 +17,7 @@ const idToColumnText: { [key in TypedColumn]: string } = {
 	done: "Done",
 };
 function Column({ id, todos, index }: Props) {
+	const [searchString] = useBoardStore((state) => [state.searchString]);
 	return (
 		<Draggable
 			draggableId={id}
@@ -42,45 +44,66 @@ function Column({ id, todos, index }: Props) {
 								<h2 className="flex justify-between font-bold text-xl p-2">
 									{idToColumnText[id]}
 
-									<span className="text-gray-500 bg-gray-200 rounded-full px-2 py-1 text-sm font-normal">
-										{todos.length}
-									</span>
+									     <span className="text-gray-500 bg-gray-200 rounded-full font-normal px-2 py-1 text-sm">
+                    {!searchString
+                      ? todos.length
+                      : todos.filter((todo) =>
+                          todo.title
+                            .toLowerCase()
+                            .includes(searchString.toLowerCase())
+                        ).length}
+                  </span>
 								</h2>
 
-								<div className="space-y-2 ">
-									{todos.map((todo, index) => (
-										<Draggable
-											key={todo.$id}
-											draggableId={todo.$id}
-											index={index}
-										>
-											{(provided) => (
-												<TodoCard
-													todo={todo}
-													index={index}
-													id={id}
-													innerRef={provided.innerRef}
-													draggableProps={provided.draggableProps}
-													dragHandleProps={provided.dragHandleProps}
-												/>
-											)}
-										</Draggable>
-									))}
-									{provided.placeholder}
+							<div className="space-y-2">
+                  {/* conditionally check if there is searchstring, if so filter and map through todos else just map */}
+                  {todos.map((todo, index) => {
+                    if (
+                      searchString &&
+                      !todo.title
+                        .toLowerCase()
+                        .includes(searchString.toLocaleLowerCase())
+                    ) {
+                      return null; // Skip rendering this todo
+                    }
 
-									<div className="flex items-end justify-end p-2">
-										<button className="text-green-500 hover:text-green-600">
-											<PlusCircleIcon className="h-10 w-10" />
-										</button>
-									</div>
-								</div>
-							</div>
-						)}
-					</Droppable>
-				</div>
-			)}
-		</Draggable>
-	);
+                    return (
+                      <Draggable
+                        key={todo.$id}
+                        draggableId={todo.$id}
+                        index={index}
+                      >
+                        {(provided) => (
+                          <TodoCard
+                            todo={todo}
+                            index={index}
+                            id={id}
+                            innerRef={provided.innerRef}
+                            draggableProps={provided.draggableProps}
+                            dragHandleProps={provided.dragHandleProps}
+                          />
+                        )}
+                      </Draggable>
+                    );
+                  })}
+                  {provided.placeholder}
+
+                  <div className="flex items-end justify-end p-2">
+                    <button
+                      // onClick={handleAddTodo}
+                      className="text-green-500 hover:text-green-600"
+                    >
+                      <PlusCircleIcon className="h-10 w-10" />
+                    </button>
+                  </div>
+                </div>
+              </div>
+            )}
+          </Droppable>
+        </div>
+      )}
+    </Draggable>
+  );
 }
 
 export default Column;
