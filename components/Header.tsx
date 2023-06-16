ebd0602c5a32d3fd41f2e43ccdcbe0bd4ecd4b92
Faisal Mohammed <m.faisal244@gmail.com>
2023-06-16 15:43:43 +0100

    Created useEffect to query chat GPT API on render

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,12 +3,32 @@ import Image from "next/image";
 import { MagnifyingGlassIcon, UserCircleIcon } from "@heroicons/react/24/solid";
 import Avatar from "react-avatar";
 import { useBoardStore } from "@/store/BoardStore";
+import { useEffect, useState } from "react";
+import fetchSuggestion from "@/lib/fetchSuggestion";
 
 function Header() {
-	const [searchString, setSearchString] = useBoardStore((state) => [
-		state.searchString,
-		state.setSearchString,
-	]);
+  const [board, searchString, setSearchString] = useBoardStore((state) => [
+    state.board,
+    state.searchString,
+    state.setSearchString,
+  ]);
+  const [suggestion, setSuggestion] = useState<string>("");
+  const [loading, setLoading] = useState<boolean>(false);
+
+  useEffect(() => {
+    if (board.columns.size === 0) return;
+    setLoading(true);
+
+    const fetchSuggestionFunc = async () => {
+      const suggestion = await fetchSuggestion(board);
+      setSuggestion(suggestion);
+      setLoading(false);
+    };
+
+    fetchSuggestionFunc();
+  }, [board]);
+
+
 	return (
 		<header>
 			<div className="flex flex-col md:flex-row items-center p-5 bg-gray-500/10 rounded-b-2xl">
@@ -50,12 +70,19 @@ function Header() {
 				</div>
 			</div>
 
-			<div className="flex items-center justify-center px-5 py-2 md:py-5">
-				<p className="flex items-center p-5 text-sm font-light pr-5 shadow-xl rounded-xl w-fit bg-white italic max-w-3xl text-[#0055D1] ">
-					<UserCircleIcon className="inline-block h-10 w-10 text-[#0055D1] mr-1" />
-					GPT is summarising your tasks for the day...
-				</p>
-			</div>
+	     <div className="flex items-center justify-center px-5 md:py-5">
+        <p className="flex items-center text-sm font-light pr-5 shadow-xl rounded-xl w-fit bg-white italic p-5 max-w-3xl text-[#0055D1]">
+          <UserCircleIcon
+            className={`inline-block h-10 w-10 text-[#0055D1] mr-1 ${
+              loading && "animate-spin"
+            }`}
+          />
+
+          {suggestion && !loading
+            ? suggestion
+            : "GPT is summarising your tasks for the day..."}
+        </p>
+      </div>
 		</header>
 	);
 }
